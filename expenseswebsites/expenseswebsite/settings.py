d637911f76cd4c8ab516e07615a7a30ea1396c4b
yuyuabdinoor <yuyuabdinoor@gmail.com>
2023-08-13 17:01:15 +0300

    changed the file name
    avoid any confusion

--- expenseswebsites/expenseswebsite/settings.py
+++ expenseswebsites/expenseswebsite/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+
 from pathlib import Path
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +79,10 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'incomeexpensesdb',
-        'USER': 'postgres',
-        'PASSWORD': '40457863',
-        'HOST' : 'localhost',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'HOST' : os.environ.get('DB_HOST'),
         
     }
 }
