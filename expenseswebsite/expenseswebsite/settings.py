75501807c620b77ab1b14f8be803910a99b5fe08
yuyuabdinoor <yuyuabdinoor@gmail.com>
2023-08-16 19:51:02 +0300

    added template file

--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'expenses'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'expenses'
+    
 ]
 
 ROOT_URLCONF = 'expenseswebsite.urls'
