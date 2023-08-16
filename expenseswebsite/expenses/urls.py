5548767e446e9d749595be5ccf475c6008214c53
yuyuabdinoor <yuyuabdinoor@gmail.com>
2023-08-16 20:52:54 +0300

    .

--- expenseswebsite/expenses/urls.py
+++ expenseswebsite/expenses/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='expense'),
+    path('', views.index, name='expenses'),
     path('add-expense', views.add_expense, name='add-expenses'),
     
 ]
